0a69b3d55e31946b73805cb3562969bd73283446
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-07-02 17:48:11 +0700

    Fix layout for add course page

--- lib/app/modules/add_course/widgets/DetailPage.dart
+++ lib/app/modules/add_course/widgets/DetailPage.dart
@@ -12,49 +12,53 @@ class DetailPage extends StatelessWidget {
   final AddCourseController controller = Get.put(AddCourseController(Get.arguments));
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Số lượng học viên",
-            style: GoogleFonts.nunitoSans(),
+    return Align(
+      alignment: Alignment.center,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Số lượng học viên",
+                style: GoogleFonts.nunitoSans(),
+              ),
+              TextField(
+                cursorColor: primaryColor,
+                decoration: primaryTextFieldDecoration,
+                controller: controller.txtStudentNum,
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Giá tiền đề nghị (vnd)",
+                style: GoogleFonts.nunitoSans(),
+              ),
+              TextField(
+                cursorColor: primaryColor,
+                decoration: primaryTextFieldDecoration,
+                controller: controller.txtCoursePrice,
+                onChanged: controller.formatCurrencyText,
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Địa điểm học",
+                style: GoogleFonts.nunitoSans(),
+              ),
+              TextField(
+                cursorColor: primaryColor,
+                decoration: primaryTextFieldDecoration,
+                controller: controller.txtCourseAddress,
+              ),
+            ],
           ),
-          TextField(
-            cursorColor: primaryColor,
-            decoration: primaryTextFieldDecoration,
-            controller: controller.txtStudentNum,
-            keyboardType: TextInputType.number,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Giá tiền đề nghị (vnd)",
-            style: GoogleFonts.nunitoSans(),
-          ),
-          TextField(
-            cursorColor: primaryColor,
-            decoration: primaryTextFieldDecoration,
-            controller: controller.txtCoursePrice,
-            onChanged: controller.formatCurrencyText,
-            keyboardType: TextInputType.number,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Địa điểm học",
-            style: GoogleFonts.nunitoSans(),
-          ),
-          TextField(
-            cursorColor: primaryColor,
-            decoration: primaryTextFieldDecoration,
-            controller: controller.txtCourseAddress,
-          ),
-        ],
+        ),
       ),
     );
   }
